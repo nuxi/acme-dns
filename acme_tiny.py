64dedcf8aaabf8246e3c3b0672d4fe7665e21d16
Jon DeVree <nuxi@vault24.org>
2018-06-09 14:01:28 -0400

    Poll challenge state before polling authz
    This is simpler than finding the right challenge object in the authz
    response.

--- acme_tiny.py
+++ acme_tiny.py
@@ -201,13 +201,16 @@ def get_crt(account_key, csr, skip_check=False, log=LOGGER, CA=PROD_CA, contact=
 
         # say the challenge is done
         _send_signed_request(challenge['url'], {}, "Error submitting challenges: {0}".format(rdomain))
+        challenge = _poll_until_not(challenge['url'], ["pending", "processing"], "Error checking challenge status for {0}".format(rdomain))
+        if challenge['status'] != "valid":
+            if 'error' in challenge:
+                raise ValueError("Challenge failed (status: {0}) for {1}:\n{2}".format(challenge['status'], rdomain, pprint.pformat(challenge['error'])))
+            else:
+                raise ValueError("Challenge failed (status: {0}) for {1}: Unknown reasons".format(challenge['status'], rdomain))
+
         authorization = _poll_until_not(auth_url, ["pending"], "Error checking challenge status for {0}".format(rdomain))
         if authorization['status'] != "valid":
-            challenge = [c for c in authorization['challenges'] if c['type'] == "dns-01"][0]
-            if 'error' in challenge:
-                raise ValueError("Challenge did not pass for {0}:\n{1}".format(rdomain, pprint.pformat(challenge['error'])))
-            else:
-                raise ValueError("Challenge did not pass for {0}: Unknown reasons".format(rdomain))
+            raise ValueError('Authorization for {0} failed: {1}'.format(rdomain, authorization))
         log.info("{0} verified!".format(domain))
 
     # finalize the order with the csr
